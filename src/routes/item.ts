af8a1a0aee30edf152b7ebb2139f8b812bdc630a
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-08-31 16:58:42 -0500

    Routes module

---
+++ src/routes/item.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+/**
+ * 
+ */
+router.get('/', (req: Request, res: Response) => {
+  res.send({
+    data: 'Aqui van los modelos'
+  })
+});
+
+export { router };
