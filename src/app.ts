6a1fdc6a06232ea420a521003216af66057b7084
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-08-31 19:47:53 -0500

    creating controllers and interacting with routes

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import db from "./config/mongo";
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
+app.use(express.json()); // Importante para permitir recibir datos
 app.use(router);
 db().then(() => console.log(`Conexion Ready`));
 app.listen(PORT, () => console.log(`Listo por el puerto: ${PORT}`));
--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,14 +1,15 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import { getItems, getItem, postItem, updateItem, deleteItem } from "../controllers/item";
 
 const router = Router();
 
 /**
  * 
  */
-router.get('/', (req: Request, res: Response) => {
-  res.send({
-    data: 'Aqui van los modelos'
-  })
-});
+router.get('/', getItems);
+router.get('/:id', getItem);
+router.post('/', postItem);
+router.put('/:id', updateItem);
+router.delete('/:id', deleteItem);
 
 export { router };
